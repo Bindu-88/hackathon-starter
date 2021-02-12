0197bd058a1b93eafc3b33c3f035b12d9503ea3b
roken47 <mradeboer7@gmail.com>
2021-02-12 15:09:09 -0600

    added the forgotten import into index.js for react-boostrap to work

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Menu.css";
 import { withAsyncAction } from "../../redux/HOCs";
+import Button from 'react-bootstrap/Button';
 
 class Menu extends React.Component {
   handleLogout = event => {
@@ -15,9 +15,9 @@ class Menu extends React.Component {
         <h1>Kwitter</h1>
         {this.props.isAuthenticated && (
           <div id="menu-links">
-            <Link to="/" onClick={this.handleLogout}>
+            <Button variant="dark" href="/" onClick={this.handleLogout}>
               Logout
-            </Link>
+            </Button>
           </div>
         )}
       </div>
